9c784f04adb27029976f43b60b1de15e81b2dbf1
Braian-92 <zamudiobraianhernan@gmail.com>
2024-04-07 14:12:33 -0300

    función MIX

--- Basico ThreeJs/27_funciones/src/shaders/fragment.glsl
+++ Basico ThreeJs/27_funciones/src/shaders/fragment.glsl
@@ -3,15 +3,7 @@ precision mediump float;
 varying vec2 v_uv;
 
 void main() {
-    //Copying uv
-    vec2 copy_uv = v_uv;
-
-    //if_condition
-    if(copy_uv.x>0.5){
-        copy_uv.x = 0.0;
-    }else {
-        copy_uv.x = 1.0;
-    }
+    vec3 mixing = mix(vec3(0.0,0.0,1.0),vec3(0.0,1.0,0.0),v_uv.x);
     
-    gl_FragColor = vec4(copy_uv, 1.0,1.0);
+    gl_FragColor = vec4(mixing,1.0);
 }
